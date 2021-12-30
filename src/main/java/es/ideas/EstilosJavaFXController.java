3cf35520bfb4b649f5407767a12d45158ebb8183
Esteban A. Giménez <estebanalfons.gimenez@murciaeduca.es>
2021-12-30 13:24:35 +0100

    IC

---
+++ src/main/java/es/ideas/EstilosJavaFXController.java
@@ -0,0 +1,15 @@
+package es.ideas;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+public class EstilosJavaFXController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    }
+
+}
